6c6e7d8b68556b43647749d1441405cf969d1327
unknown <niyonkurutresor.com>
2023-06-20 23:27:07 +0200

    delete post

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -6,6 +6,8 @@ import { PostsController } from '../controller/postsController.js';
 const routes = express();
 
 routes.post('/all', PostsController.getPosts);
+routes.delete('/:id', PostsController.deletePost);
 routes.post('/:id/update', PostsController.updatePost);
+
 
 export default routes;
